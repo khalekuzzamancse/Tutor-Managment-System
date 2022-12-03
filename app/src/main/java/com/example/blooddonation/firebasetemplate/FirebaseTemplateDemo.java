29efff079cef5fa61f9795f2cec04fa61cfddc28
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2022-12-03 16:09:03 +0600

    okay

--- app/src/main/java/com/example/blooddonation/firebasetemplate/FirebaseTemplateDemo.java
+++ app/src/main/java/com/example/blooddonation/firebasetemplate/FirebaseTemplateDemo.java
@@ -10,42 +10,37 @@ public class FirebaseTemplateDemo {
 
     public void demoGetAllDocumentName() {
         FirebaseCustom firebaseCustom = new DatabaseFetch();
-        //collectionName=DistrictList
-        firebaseCustom.getAllDocumentName("DistrictList", new CallbackDatabase() {
-            @Override
-            public void getList(List<String> list) {
-                Log.i("Fetected-AllDocumentName" + " of -> " + "DistrictList", String.valueOf(list));
-            }
-        });
-        //fetching using lamda
-        //        new DatabaseFetch().getAllDocumentName("DistrictList", list -> {
-//            Log.i("Fetched-ClassList", String.valueOf(list));
+//        //collectionName=DistrictList
+//        firebaseCustom.getAllDocumentName("DistrictList", new CallbackDatabase() {
+//            @Override
+//            public void getList(List<String> list) {
+//                Log.i("Fetched-AllDocumentName" + " of -> " + "DistrictList", String.valueOf(list));
+//            }
 //        });
+        //lambda-version for multiple statement
+//        firebaseCustom.getAllDocumentName("DistrictList", list -> {
+//            Log.i("Fetched-AllDocumentName" + " of -> " + "DistrictList", String.valueOf(list)));
+//        });
+
+        //fetching using lambda,removing { },because of single statement
+        firebaseCustom.getAllDocumentName("DistrictList", list ->
+                Log.i("Fetched-AllDocumentName" + " of -> " + "DistrictList", String.valueOf(list)));
 
     }
 
     public void demoGetDocument() {
         FirebaseCustom firebaseCustom = new DatabaseFetch();
         //collectionName=DistrictList ,documentId/Name=Dhaka
-        firebaseCustom.getDocument("DistrictList", "Dhaka", new CallbackDocument() {
-            @Override
-            public void getDoc(HashMap<String, Object> doc) {
-                Log.i("Fetected-Document" + " of -> " + "DistrictList ,Document ->" + "Dhaka", String.valueOf(doc));
-            }
-        });
+        firebaseCustom.getDocument("DistrictList", "Dhaka", doc ->
+                Log.i("Fetched-Document" + " of -> " + "DistrictList ,Document ->" + "Dhaka", String.valueOf(doc)));
     }
 
     public void demoFetDocumentField() {
         FirebaseCustom firebaseCustom = new DatabaseFetch();
         //collectionName=DistrictList ,documentId/Name=Dhaka
         //documentName/Id=jahidrana190153cse@gmail.com ,fieldName=Name
-        firebaseCustom.getDocumentField("UserInfo", "jahidrana190153cse@gmail.com", "Name", new
-                CallBackDocumentField() {
-                    @Override
-                    public void getField(Object fieldValue) {
-                        Log.i("Fetected-Document" + " of -> " + "DistrictList ,Document=" + "jahidrana190153cse@gmail.com ,FieldValue ->", String.valueOf(fieldValue));
-                    }
-                });
+        firebaseCustom.getDocumentField("UserInfo", "jahidrana190153cse@gmail.com", "Name", fieldValue ->
+                Log.i("Fetched-Document" + " of -> " + "DistrictList ,Document=" + "jahidrana190153cse@gmail.com ,FieldValue ->", String.valueOf(fieldValue)));
 
     }
 
@@ -76,6 +71,8 @@ public class FirebaseTemplateDemo {
         FirebaseCustom firebaseCustom = new DatabaseFetch();
         //collectionName=ClassInfo ,documentId/Name=one
         //field(array/list)Name=subjectList ,arrayNewElement=Math
+        //Note:array element can be any type(primitive or complex) as defined in the database
+        //where we use array filed data type string so we are going to pass a string("Math)
         firebaseCustom.updateArrayItem("ClassInfo", "one", "subject", "Math");
     }
 
@@ -83,6 +80,8 @@ public class FirebaseTemplateDemo {
         FirebaseCustom firebaseCustom = new DatabaseFetch();
         //collectionName=ClassInfo ,documentId/Name=one
         //field(array/list)Name=subjectList ,arrayNewElement=Math
+        //Note:array element can be any type(primitive or complex) as defined in the database
+        //where we use array filed data type string so we are going to pass a string("Math)
         firebaseCustom.deleteArrayItem("ClassInfo", "one", "subject", "Math");
     }
 
